1e77b898dedc899a315adec9b2cd43328fac73cd
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-06-08 15:19:09 -0500

    created MovieDetail model

---
+++ Models/TMDB/MovieDetail.cs
@@ -0,0 +1,170 @@
+﻿namespace MovieProWonder.Models.TMDB
+{
+    #region Rootobject
+    public class Rootobject
+    {
+        public bool adult { get; set; }
+        public string backdrop_path { get; set; }
+        public Belongs_To_Collection belongs_to_collection { get; set; }
+        public int budget { get; set; }
+        public Genre[] genres { get; set; }
+        public string homepage { get; set; }
+        public int id { get; set; }
+        public string imdb_id { get; set; }
+        public string original_language { get; set; }
+        public string original_title { get; set; }
+        public string overview { get; set; }
+        public float popularity { get; set; }
+        public string poster_path { get; set; }
+        public Production_Companies[] production_companies { get; set; }
+        public Production_Countries[] production_countries { get; set; }
+        public string release_date { get; set; }
+        public int revenue { get; set; }
+        public int runtime { get; set; }
+        public Spoken_Languages[] spoken_languages { get; set; }
+        public string status { get; set; }
+        public string tagline { get; set; }
+        public string title { get; set; }
+        public bool video { get; set; }
+        public float vote_average { get; set; }
+        public int vote_count { get; set; }
+        public Credits credits { get; set; }
+        public Images images { get; set; }
+        public Videos videos { get; set; }
+        public Release_Dates release_dates { get; set; }
+    }
+    #endregion
+    #region Belongs to collection
+    public class Belongs_To_Collection
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string poster_path { get; set; }
+        public string backdrop_path { get; set; }
+    }
+    #endregion
+    #region Credits
+    public class Credits
+    {
+        public Cast[] cast { get; set; }
+        public Crew[] crew { get; set; }
+    }
+    #endregion
+    #region Cast
+    public class Cast
+    {
+        public bool adult { get; set; }
+        public int gender { get; set; }
+        public int id { get; set; }
+        public string known_for_department { get; set; }
+        public string name { get; set; }
+        public string original_name { get; set; }
+        public float popularity { get; set; }
+        public string profile_path { get; set; }
+        public int cast_id { get; set; }
+        public string character { get; set; }
+        public string credit_id { get; set; }
+        public int order { get; set; }
+    }
+    #endregion
+    #region Crew
+    public class Crew
+    {
+        public bool adult { get; set; }
+        public int gender { get; set; }
+        public int id { get; set; }
+        public string known_for_department { get; set; }
+        public string name { get; set; }
+        public string original_name { get; set; }
+        public float popularity { get; set; }
+        public string profile_path { get; set; }
+        public string credit_id { get; set; }
+        public string department { get; set; }
+        public string job { get; set; }
+    }
+
+    #endregion
+    #region Images
+    public class Images
+    {
+        public object[] backdrops { get; set; }
+        public object[] logos { get; set; }
+        public object[] posters { get; set; }
+    }
+    #endregion
+    #region Videos
+    public class Videos
+    {
+        public VideoResult[] results { get; set; }
+    }
+    #endregion
+    #region VideoResult
+    public class VideoResult
+    {
+        public string iso_639_1 { get; set; }
+        public string iso_3166_1 { get; set; }
+        public string name { get; set; }
+        public string key { get; set; }
+        public string published_at { get; set; }
+        public string site { get; set; }
+        public int size { get; set; }
+        public string type { get; set; }
+        public bool official { get; set; }
+        public string id { get; set; }
+    }
+    #endregion
+    #region ReleaseDates
+    public class Release_Dates
+    {
+        public ReleaseResult[] results { get; set; }
+    }
+    #endregion
+    #region ReleaseResult
+    public class ReleaseResult
+    {
+        public string iso_3166_1 { get; set; }
+        public Release_Date[] release_dates { get; set; }
+    }
+    #endregion
+    #region ReleaseDate
+    public class Release_Date
+    {
+        public string certification { get; set; }
+        public string iso_639_1 { get; set; }
+        public string note { get; set; }
+        public string release_date { get; set; }
+        public int type { get; set; }
+    }
+    #endregion
+    #region Genre
+    public class Genre
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+    #endregion
+    #region ProductionCompanies
+    public class Production_Companies
+    {
+        public int id { get; set; }
+        public string logo_path { get; set; }
+        public string name { get; set; }
+        public string origin_country { get; set; }
+    }
+    #endregion
+    #region Production_Countries
+    public class Production_Countries
+    {
+        public string iso_3166_1 { get; set; }
+        public string name { get; set; }
+    }
+    #endregion
+    #region SpokenLanguages
+    public class Spoken_Languages
+    {
+        public string english_name { get; set; }
+        public string iso_639_1 { get; set; }
+        public string name { get; set; }
+    }
+    #endregion
+}
