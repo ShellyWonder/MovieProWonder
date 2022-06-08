627352bd4a16ea7de52e5dab5f97485c6e606a90
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-06-08 18:44:40 -0500

    created MovieCollection model

---
+++ Models/MovieCollection.cs
@@ -0,0 +1,22 @@
+﻿using MovieProWonder.Models.Database;
+using System.Collections.ObjectModel;
+
+namespace MovieProWonder.Models
+{
+    public class MovieCollection
+    {
+        //primary key
+        public int Id { get; set; }
+       
+        //two foreign keys
+        public int CollectionId { get; set; }
+        public int MovieId { get; set; }
+
+        public int Order { get; set; }
+
+        //store entire record pointed to foreign key
+        public Collection Collection { get; set; }
+
+        public Movie Movie { get; set; }
+    }
+}
