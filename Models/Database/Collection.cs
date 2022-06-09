5a6698605fcae368c25b2a568f24c4b78879dafd
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-06-08 19:07:58 -0500

    updated model with nav property

--- Models/Database/Collection.cs
+++ Models/Database/Collection.cs
@@ -6,5 +6,10 @@
         public string Name { get; set; }
         public string Description { get; set; }
 
+        //collection of collections--child property
+
+        public ICollection<MovieCollection> MovieCollections  { get; set; } = new HashSet<MovieCollection>();
+
+
     }
 }
