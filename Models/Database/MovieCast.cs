738ed4f778c63c1114239b908353a43833660b5c
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-06-08 19:02:40 -0500

    building out MovieCrew and MovieCast models

---
+++ Models/Database/MovieCast.cs
@@ -0,0 +1,27 @@
+﻿namespace MovieProWonder.Models.Database
+{
+    //stores credit data returned by API
+    public class MovieCast
+    {
+        public int Id { get; set; }
+        
+        //foreign key links cast member to movie
+        public int MovieId { get; set; }
+       
+        //retrives detailed info on cast member
+        public int CastID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Department { get; set; }  
+        public string Character { get; set; }   
+        public string ImageUrl { get; set; }    
+
+        //navigational property
+        public Movie Movie { get; set; }    
+
+
+        
+
+    }
+}
---
+++ Models/Database/MovieCrew.cs
@@ -0,0 +1,22 @@
+﻿namespace MovieProWonder.Models.Database
+{
+    public class MovieCrew
+    {
+        public int Id { get; set; } 
+
+        public int MovieId { get; set; }  
+        
+        //supplied by TMDB API
+        public int CrewID { get; set; } 
+
+        public string Department { get; set; }
+        public string Name { get; set; }   
+        
+        public string Job { get; set; }
+
+        public string ImageUrl { get; set; }    
+
+        //nav prop that stores entire record for display
+        public Movie Movie { get; set; }
+    }
+}
