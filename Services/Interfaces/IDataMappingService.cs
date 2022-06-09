a01506cb61e5135529c2241c5dfddbae29639ed9
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-06-08 20:32:22 -0500

    creating interfaces

---
+++ Services/Interfaces/IDataMappingService.cs
@@ -0,0 +1,11 @@
+﻿using MovieProWonder.Models.Database;
+using MovieProWonder.Models.TMDB;
+
+namespace MovieProWonder.Services.Interfaces
+{
+    public interface IDataMappingService
+    {
+        Task<Movie> MapMovieDetailAsync(MovieDetail movie);
+        ActorDetail MapActorDetail(ActorDetail actor);
+    }
+}
---
+++ Services/Interfaces/IRemoteMovieService.cs
@@ -0,0 +1,12 @@
+﻿using MovieProWonder.Enums;
+using MovieProWonder.Models.TMDB;
+
+namespace MovieProWonder.Services.Interfaces
+{
+    public interface IRemoteMovieService
+    {
+        Task<MovieDetail> MovieDetailAsync(int id);
+        Task<MovieSearch> SearchMoviesAsync(MovieCategory category, int count);
+        Task<ActorDetail> ActorDetailAsync(int id);
+    }
+}
