34a4d3269960245d0bf1027bc1a3af19f218023b
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-06-08 20:08:31 -0500

    created landingpage view model

---
+++ Models/ViewModels/LandingPageVM.cs
@@ -0,0 +1,15 @@
+﻿using MovieProWonder.Models.Database;
+using MovieProWonder.Models.TMDB;
+using System.Collections.ObjectModel;
+
+namespace MovieProWonder.Models.ViewModels
+{
+    public class LandingPageVM
+    {
+        public List<Collection>CustomCollections { get; set; }
+        public MovieSearch NowPlaying { get; set; }
+        public MovieSearch Popular { get; set; }
+        public MovieSearch TopRated { get; set; }
+        public MovieSearch Upcoming { get; set; }
+    }
+}
