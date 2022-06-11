bf22390d4c51fca4af448ef8c5ce58bbcd9f6e53
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-06-10 20:43:07 -0500

    customized home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieProWonder.Data;
+using MovieProWonder.Enums;
 using MovieProWonder.Models;
+using MovieProWonder.Models.ViewModels;
+using MovieProWonder.Services.Interfaces;
 using System.Diagnostics;
 
 namespace MovieProWonder.Controllers
@@ -7,17 +12,39 @@ namespace MovieProWonder.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly IRemoteMovieService _tmdbMovieService;
 
-        public HomeController(ILogger<HomeController> logger)
+        #region constructor
+        public HomeController(ILogger<HomeController> logger,
+                                ApplicationDbContext context,
+                                IRemoteMovieService tmdbMovieService)
         {
             _logger = logger;
+            _context = context;
+            _tmdbMovieService = tmdbMovieService;
         }
-
-        public IActionResult Index()
+        #endregion
+        #region Index
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
+            const int count = 16;
+            var data = new LandingPageVM()
+            {
+                CustomCollections = await _context.Collection
+                                            .Include(c => c.MovieCollections)
+                                            .ThenInclude(mc => mc.Movie)
+                                            .ToListAsync(),
 
+                NowPlaying = await _tmdbMovieService.SearchMoviesAsync(MovieCategory.now_playing, count),
+                Popular = await _tmdbMovieService.SearchMoviesAsync(MovieCategory.popular, count),
+                TopRated = await _tmdbMovieService.SearchMoviesAsync(MovieCategory.top_rated, count),
+                Upcoming = await _tmdbMovieService.SearchMoviesAsync(MovieCategory.upcoming, count)
+            };
+            return View(data);
+        }
+        #endregion
+        #region Privacy
         public IActionResult Privacy()
         {
             return View();
@@ -28,5 +55,6 @@ namespace MovieProWonder.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        #endregion
     }
 }
